821125df4054b976f29d623f2ec234fdaa7be412
dandradeTHELOT <dandrade@thelotent.com>
2025-04-24 21:39:39 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,5 +7,21 @@ const firebaseConfig = {
   appId: "1:713817230380:web:762944cb969becbf98d589",
   measurementId: "G-HQ83YS382L"
 };
+
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Verificar que Firebase se ha inicializado correctamente
+firebase.firestore().enablePersistence()
+  .then(function() {
+    console.log("Conexión a Firestore exitosa");
+  })
+  .catch(function(err) {
+    if (err.code === 'failed-precondition') {
+      console.error("La persistencia no está habilitada debido a múltiples pestañas abiertas.");
+    } else if (err.code === 'unimplemented') {
+      console.error("La persistencia no está disponible en este navegador.");
+    } else {
+      console.error("Error al conectar a Firestore:", err);
+    }
+  });
